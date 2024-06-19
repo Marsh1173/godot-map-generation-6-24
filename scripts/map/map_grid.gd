fac8bd81d9ada4309f19a73a0ae69119e1854cd5
marsh1173 <natehroylance@gmail.com>
2024-06-19 15:21:01 -0600

    first commit

---
+++ scripts/map/map_grid.gd
@@ -0,0 +1,12 @@
+class_name MapGrid
+
+var size: Vector2
+var gridmap: Array[Array] = []
+
+func _init(_size: Vector2i):
+	size = _size
+	
+	for i in size.x:
+		gridmap.append(Array([] )
+	gridmap.
+
